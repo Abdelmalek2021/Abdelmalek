60602c898124ca375e2ba28a185d44589bf79397
Abdelmalek2021 <82372093+Abdelmalek2021@users.noreply.github.com>
2021-04-13 11:01:13 +0100

    nouvelle version

---
+++ script nouvelle version.js
@@ -0,0 +1,54 @@
+
+
+/*function calcul(num1,num2){
+    if (num2 !==0) {
+        return ( num1/num2)
+    } else{
+        console.log( "la division par 0 est imposssible")
+    }
+}
+var numero1 =1.5;
+var numero2 =0.5;
+
+console.log(numero1+ "/" +numero2+ "=" + calcul(numero1,numero2))
+console.log("la division par 0 est impossible car ça vous nous donner infinity".length)
+
+var etudiants=[,eleve1,,eleve2,eleve3]
+console.log(etudiants.length)*/
+
+
+/*for(i=2;i<10;i++)
+console.log("bonjour");*/
+
+/*function carre(b) {
+	return b*b;
+}
+console.log(carre(59987967587));*/
+
+
+/*function check(num1,num2) {
+    sum=num1+num2;
+if (sum<100) console.log("vrais");
+else console.log("faux") }
+
+check(5,20);
+check(60,55);
+
+check(99,0);*/
+
+/*function verif(num1,num2){
+
+    if (num1===num2) {
+    console.log("true") }
+    else
+    {console.log("false")}
+}
+
+verif(3,6);
+verif(1,'1');
+verif(1,1);*/
+
+
+Tableau tab[i,j]
+for (i=0; i<tab.lentgth;i++)
+console.log (tab[i])
